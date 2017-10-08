c20e42c59c73d2dad451171429cb3d58cc6a8f51
AleksandrPeshcherov <kawalot@gmail.com>
2017-10-08 22:08:38 +0300

    Section 4: 18/19 Completed

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^items/', include('menus.urls', namespace='menus')),
+    url(r'^u/', include('profiles.urls', namespace='profiles')),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
