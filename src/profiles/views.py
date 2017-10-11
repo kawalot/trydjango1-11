0f7bde2903619159556b21bef18b232e4e177bc8
AleksandrPeshcherov <kawalot@gmail.com>
2017-10-11 22:43:00 +0300

    Section 5: 2/5 Completed

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,13 +1,24 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import DetailView, View
 from django.http import Http404
 
 from restaurants.models import Restaurant
 from menus.models import Item
+
+from .models import Profile
 # Create your views here.
 
 User = get_user_model()
+
+class ProfileFollowToggle(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        #print(request.POST)
+        user_to_toggle = request.POST.get("username")
+        profile_, is_following = Profile.objects.toggle_follow(request.user, user_to_toggle)
+        print(is_following)
+        return redirect(f"/u/{ profile_.user.username }/")
 
 class ProfileDetailView(DetailView):
     template_name = 'profiles/user.html'
@@ -21,6 +32,10 @@ class ProfileDetailView(DetailView):
     def get_context_data(self, *args, **kwargs):
         context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
         user = context['user']
+        is_following = False
+        if user.profile in self.request.user.is_following.all():
+            is_following = True
+        context['is_following'] = is_following
         query = self.request.GET.get('q')
         items_exists = Item.objects.filter(user=user).exists()
         qs = Restaurant.objects.filter(owner=user).search(query)
