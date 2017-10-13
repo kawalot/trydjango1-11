66e34aae0df3363a6b7d52cf44c9c0e786ab3170
AleksandrPeshcherov <kawalot@gmail.com>
2017-10-13 22:03:46 +0300

    Section 5: 4/5 Completed

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -17,14 +17,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from menus.views import HomeView
-from profiles.views import ProfileFollowToggle
+from profiles.views import ProfileFollowToggle, RegisterView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^items/', include('menus.urls', namespace='menus')),
     url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
