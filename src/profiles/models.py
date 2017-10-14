b798ae5744d624ec37d35bb19ba3631811725558
AleksandrPeshcherov <kawalot@gmail.com>
2017-10-14 18:05:43 +0300

    Section 5: 5/5 Completed

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
-
+from .utils import code_generator
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
@@ -22,6 +24,7 @@ class Profile(models.Model):
     user        = models.OneToOneField(User)
     followers   = models.ManyToManyField(User, related_name='is_following', blank=True)
     #following   = models.ManyToManyField(User, related_name='following', blank=True)
+    activation_key = models.CharField(max_length=120, blank=True, null=True)
     activated   = models.BooleanField(default=False)
     timestamp   = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
@@ -33,7 +36,26 @@ class Profile(models.Model):
 
     def send_activation_email(self):
         print("Activation")
-        pass
+        if not self.activated:
+            self.activation_key = code_generator() # gen key
+            self.save()
+            path_ = reverse('activate', kwargs={"code": self.activation_key})
+            subject = 'Activate Account'
+            from_email = settings.DEFAULT_FROM_EMAIL
+            message = f'Activate your account here: {path_}'
+            recipient_list = [self.user.email]
+            html_message = f'<p>Activate your account here: {path_}</p>'
+            print(html_message)
+            # sent_mail = send_mail(
+            #         subject, 
+            #         message, 
+            #         from_email, 
+            #         recipient_list, 
+            #         fail_silently=False, 
+            #         html_message=html_message
+            # )
+            sent_mail = False
+            return sent_mail
 
 def post_save_user_receiver(sender, instance, created, *args, **kwargs):
     if created:
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -12,14 +12,29 @@ from .models import Profile
 
 User = get_user_model()
 
+def activate_user_view(request, code=None, *args, **kwargs):
+    if code:
+        qs = Profile.objects.filter(activation_key=code)
+        if qs.exists() and qs.count() == 1:
+            profile = qs.first()
+            if not profile.activated:
+                user_ = profile.user
+                user_.is_active = True
+                user_.save()
+                profile.activated=True
+                profile.activation_key=None
+                profile.save()
+                return redirect("/login")
+    return redirect("/login")
+
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = '/'
 
     def dispatch(self, *args, **kwargs):
-        if self.request.user.is_authenticated():
-            return redirect("/logout/")
+        # if self.request.user.is_authenticated():
+        #     return redirect("/logout/")
         return super(RegisterView, self).dispatch(*args, **kwargs)
 
 class ProfileFollowToggle(LoginRequiredMixin, View):
